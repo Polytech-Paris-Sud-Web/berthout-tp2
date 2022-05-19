7abcb09185830fe5a344136902f244a2ea9f7c23
Thurstag <sashaberthout@hotmail.com>
2022-05-19 10:05:02 +0200

    [NEW] Add PWA support
    Update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ArticleComponent} from './article/article.component';
@@ -9,10 +8,12 @@ import {AddArticleService, ArticleService} from "./article.service";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {ArticleCreationComponent} from './article-creation/article-creation.component';
-import { SingleArticleComponent } from './single-article/single-article.component';
-import { HomeComponent } from './home/home.component';
-import { AllArticlesComponent } from './all-articles/all-articles.component';
-import { AuthorComponent } from './author/author.component';
+import {SingleArticleComponent} from './single-article/single-article.component';
+import {HomeComponent} from './home/home.component';
+import {AllArticlesComponent} from './all-articles/all-articles.component';
+import {AuthorComponent} from './author/author.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,13 @@ import { AuthorComponent } from './author/author.component';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     ArticleService,
